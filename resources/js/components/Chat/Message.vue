831ae77c994b0e38c01b9ab4b74d9ff50c8d4060
Trương Minh Phung <minhphung485@gmail.com>
2024-11-21 15:24:01 +0700

    up

--- resources/js/components/Chat/Message.vue
+++ resources/js/components/Chat/Message.vue
@@ -74,7 +74,7 @@
         @click="toggleEmojiPicker"
         class="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
       >
-          <FaceSmileIcon class="h-6 w-6 text-blue-500" />
+          <FaceSmileIcon class="h-8 w-8 text-blue-500" />
       </button>
       <!-- Emoji Picker -->
       <div
